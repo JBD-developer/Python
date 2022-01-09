ed1c7e9e38f3199e9763637dbae83f9574d3f685
JBD <lio222@hanmail.net>
2022-01-09 19:54:02 +0900

    2021-01-09 Python Start

---
+++ Inflearn_Study/Ex01/Python01.py
@@ -0,0 +1,47 @@
+###############################
+# TITLE : Variable
+# CREATE DATE : 2022-01-09
+# CREATOR : J
+# MODIIFIER :
+# MODIFY DATE
+# VERSIOM : 1.0.0
+###############################
+
+num1 = 200
+num2 = 300
+
+print(type(num1))
+print(type(num2))
+print('Before num1 : ',num1, 'num2 : ',num2)
+
+numTemp = num1
+num1 = num2
+num2 = numTemp
+print('After num1 : ',num1, 'num2 : ',num2)
+
+peopleNum = input("사용자의 인원을 입력해주세요")
+chickens = int(peopleNum)
+beer = int(peopleNum) * 2
+cake =  int(peopleNum) * 3
+
+print("chickens Number : ",chickens)
+print("Beers Number : ",beer)
+print("Cakes Number : ", cake)
+
+numX = int(input("First Integer "))
+numY = int(input("Second Integer "))
+sum = numX + numY
+sub = numX - numY
+mul = numX * numY
+div = numX / numY
+print(sum)
+print(sub)
+print(mul)
+print(div)
+
+numMoney = 5000
+numCandyPrice = 120
+maxCandy = numMoney // numCandyPrice
+reminder = numMoney % numCandyPrice
+print(maxCandy)
+print(reminder)
---
+++ Inflearn_Study/Ex01/Python02.py
@@ -0,0 +1,38 @@
+###############################
+# TITLE : Operator
+# CREATE DATE : 2022-01-09
+# CREATOR : J
+# MODIIFIER :
+# MODIFY DATE
+# VERSIOM : 1.0.0
+###############################
+
+from math import *
+
+print(sqrt(3.0))
+print(sqrt(4))
+print(pow(3, 4))
+
+print(3 + 4)
+print(3.14 * 5.0 * 5.0)
+print(1 + (4 * 8) - (10 // 2))
+print(10 // 3)
+print(10 / 3)
+print(10 % 2)
+print(10 ** 2)
+
+# iTime = int(input("정수를 입력해주세요"))
+# iSecond = iTime % 60
+# iMinute = (iTime // 60) % 60
+# iHour = (iTime // 60) // 60
+# print('Hour : ', iHour ,'iMinute : ', iMinute, 'iSecond : ', iSecond )
+
+numX = 2
+numY = (3 *( numX ** 2)) + (7.0 * numX)  + 9.0
+print(numY)
+
+print(abs(-12232))
+print(round(1.2232, 2))
+
+print(max(1, 3, 4, 405, 502))
+print(min(1, 23, 3, -1 , 45))
